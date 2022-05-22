251dfee17c2036e0c63a997785c96ec20ba6043c
hoqradwan <92720128+hoqradwan@users.noreply.github.com>
2022-05-23 01:09:21 +0600

    parts detail api created

--- index.js
+++ index.js
@@ -1,33 +1,46 @@
-const express = require('express');
-const cors = require('cors');
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uaqt4.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
+async function run() {
+  try {
+    await client.connect();
+    console.log("db connected");
+    const partsCollection = client.db("moto_industrial").collection("parts");
 
-async function run(){
-    try{
-        await  client.connect();
-        console.log('db connected')
-    }
-    finally{
-
-    }
+    app.get("/parts", async (req, res) => {
+      const parts = await partsCollection.find().toArray();
+      res.send(parts);
+    });
+    app.get("/parts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await partsCollection.findOne(query);
+      res.send(result);
+    });
+  } finally {
+  }
 }
-run().catch(console.dir)
-app.get('/', (req,res)=>{
-   res.send('Hello from moto industrial')
-})
+run().catch(console.dir);
+app.get("/", (req, res) => {
+  res.send("Hello from moto industrial");
+});
 
-app.listen(port, ()=>{
-    console.log(`moto is listening to ${port}`)
-})
+app.listen(port, () => {
+  console.log(`moto is listening to ${port}`);
+});
